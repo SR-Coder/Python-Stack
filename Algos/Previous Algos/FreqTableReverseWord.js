419725c58bb64e16c2223f0b7481fa67aa1f13d5
SR-Coder <reederje@gmail.com>
2021-03-15 10:10:46 -0700

    Daily Algos

--- Algos/Previous Algos/FreqTableReverseWord.js
+++ Algos/Previous Algos/FreqTableReverseWord.js
@@ -32,12 +32,41 @@ const expected3 = {};
  * @return {Object<string, number>} A frequency table where the keys are items
  *    from the given arr and the values are the amnt of times that item occurs.
  */
-function frequencyTableBuilder(arr) {}
+ function frequencyTableBuilder(arr) {
+  const freqTable = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const str = arr[i];
+
+    if (freqTable.hasOwnProperty(str) === false) {
+      // first occurrence found
+      freqTable[str] = 1;
+    } else {
+      freqTable[str]++;
+    }
+  }
+  return freqTable;
+}
+
+function frequencyTableBuilder2(arr) {
+  const freqTable = {};
+
+  for (const str of arr) {
+    if (freqTable.hasOwnProperty(str) === false) {
+      // first occurrence found
+      freqTable[str] = 1;
+    } else {
+      freqTable[str]++;
+    }
+  }
+
+  return freqTable;
+}
 
 module.exports = { frequencyTableBuilder };
 
 /*****************************************************************************/
-
+// BONUS!!!!!
   
 /* 
   Reverse Word Order
@@ -57,7 +86,44 @@ const expected1 = "test a is This";
  * @return {string} The given string with the word order reversed but the words
  *    themselves are not reversed.
  */
-function reverseWordOrder(wordsStr) {}
+
+//  1. split string to array
+//  2. reverse array - loop across array and change index of each item
+//  3. push each item in array to a new string, separated by space
+
+function reverseWordOrder(wordsStr) {
+  // if all spaces
+  if (wordsStr == false) {
+    return wordsStr;
+  }
+
+  let currWord = "";
+  let reversedWordOrder = "";
+
+  for (let i = wordsStr.length - 1; i >= 0; --i) {
+    if (wordsStr[i] !== " ") {
+      // prepend so the Word itself is not reversed by looping backWords
+      currWord = wordsStr[i] + currWord;
+    }
+    // space found
+    else {
+      // add a space in front of the word, except on first word
+      if (reversedWordOrder.length > 0) {
+        currWord = " " + currWord;
+      }
+
+      reversedWordOrder += currWord;
+      currWord = "";
+    }
+  }
+
+  // no space at end of string means we didn't add the last word
+  if (currWord.length > 0) {
+    reversedWordOrder += " " + currWord;
+  }
+  return reversedWordOrder;
+}
+
 
 module.exports = { reverseWordOrder };
 
