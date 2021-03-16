f419754160371f9091359844324f5b22ab39a19a
SR-Coder <reederje@gmail.com>
2021-03-16 10:15:09 -0700

    Daily Algo Solution

--- Algos/W2D2.js
+++ Algos/W2D2.js
@@ -18,7 +18,39 @@ const expected2 = "helo";
  * @param {string} str A string that may contain duplicates.
  * @return {string} The given string with any duplicate characters removed.
  */
-function stringDedupe(str) {}
+/**
+ * - Time: O(n) linear.
+ * - Space: O(2n) -> O(n) linear.
+ */
+ function stringDedupe(str) {
+  let distinctStr = "";
+  const seen = {};
+
+  // loop backwards to include last occurrence
+  for (let i = str.length - 1; i >= 0; --i) {
+    if (!seen[str[i]]) {
+      distinctStr = str[i] + distinctStr;
+      seen[str[i]] = true;
+    }
+  }
+  return distinctStr;
+}
+
+/**
+ * Keeps first occurrence, no hash table approach.
+ * - Time: O(n^2) quadratic due to .include being nested loop.
+ * - Space: O(n) linear.
+ */
+function strDedupe(str) {
+  let distinctStr = "";
+
+  for (const char of str) {
+    if (distinctStr.includes(char) === false) {
+      distinctStr += char;
+    }
+  }
+  return distinctStr;
+}
 
 module.exports = { stringDedupe };
 
@@ -48,7 +80,70 @@ const expected3 = "cba fed ihg";
  * @param {string} str Contains space separated words.
  * @return {string} The given string with each word's letters reversed.
  */
-function reverseWords(str) {}
+/**
+ * - Time: O(2n) -> O(n) linear, despite there being a nested loop,
+ *    both loops amount to looping over every char in wordsStr, which is 'n'
+ *    .split is 1n, and the two for loops are 1n, so 1n + 1n = 2n.
+ * - Space: O(2n) -> O(n) linear, the input is stored twice, in words array and
+ *    in wordsReversed.
+ */
+ function reverseWordsSplit(wordsStr) {
+  const words = wordsStr.split(" ");
+  let wordsReversed = "";
+
+  for (const word of words) {
+    let reversedWord = "";
+
+    for (let i = word.length - 1; i >= 0; --i) {
+      reversedWord += word[i];
+    }
+
+    // add a space in front of word if it's not the first word
+    if (wordsReversed.length > 0) {
+      reversedWord = " " + reversedWord;
+    }
+    wordsReversed += reversedWord;
+  }
+  return wordsReversed;
+}
+
+/**
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
+ */
+function reverseWords(wordsStr) {
+  let reversedWord = "";
+  let wordsReversed = "";
+
+  for (const char of wordsStr) {
+    if (char !== " ") {
+      // prepend to reverse it
+      reversedWord = char + reversedWord;
+    }
+    // space found
+    else {
+      // add space in front of word if not first word added
+      if (wordsReversed.length > 0) {
+        reversedWord = " " + reversedWord;
+      }
+
+      wordsReversed += reversedWord;
+      reversedWord = ""; // reset
+    }
+  }
+
+  // last word wasn't added if there are no trailing spaces at the end to cause it to be added
+  if (reversedWord.length > 0) {
+    // to avoid adding a space when it's the only word
+    if (wordsReversed.length !== 0) {
+      reversedWord = " " + reversedWord;
+    }
+    wordsReversed += reversedWord;
+  }
+
+  return wordsReversed;
+}
+
 
 module.exports = { reverseWords };
 
