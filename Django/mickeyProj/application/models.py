a2a678a3933a896247878093d98edbd627614c6b
SR-Coder <reederje@gmail.com>
2021-03-18 11:12:17 -0700

    Daily Code

--- Django/mickeyProj/application/models.py
+++ Django/mickeyProj/application/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     password = models.CharField(max_length=60)
     created_at = models.DateTimeField(auto_now_add=True) # (Automatically Created)
     updated_at = models.DateTimeField(auto_now=True) # (Automatically Created)
+    # reviews.all() (phantom property)
 
     def __str__(self):
         s = '\n'
@@ -18,3 +19,23 @@ class User(models.Model):
         s += f"email: {self.email}\n"
         return s
 
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # (Automatically Created)
+    updated_at = models.DateTimeField(auto_now=True) # (Automatically Created)
+
+    reviewer = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+
+
+# user1 = User.objects.create(first_name='Shayan', last_name='Valaie', email='s@v.com', password='asdfasdf')
+# user1.first_name # returns 'Shayan'
+
+# review1 = Review.objects.create(rating=7.5, content='Looks Good', reviewer=user1)
+# review2 = Review.objects.create(rating=1.5, content='Looks bad', reviewer=user1)
+
+# review1.content # returns 'Looks Good'
+# review1.reviewer # retuns the whole user object that is related
+
+# user1.reviews.all() # returns a list of reviews [review1, review2]
