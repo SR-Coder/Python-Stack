7223c5c968a0271206d78b1d2154cfc7b0a31908
SR-Coder <reederje@gmail.com>
2021-03-24 10:04:36 -0700

    Daily Algos

---
+++ Algos/arrDdMode.js
@@ -0,0 +1,205 @@
+/* 
+  Given a SORTED array of integers, dedupe the array 
+  Because array elements are already in order, all duplicate values will be grouped together.
+  Ok to use a new array
+  Bonus: do it in O(n) time (no nested loops, new array ok)
+  Bonus: Do it in-place (no new array)
+  Bonus: Do it in-place in O(n) time and no new array
+  Bonus: Keep it O(n) time even if it is not sorted
+*/
+
+const nums1 = [1, 1, 1, 1];
+const expected1 = [1];
+
+const nums2 = [1, 1, 2, 2, 3, 3];
+const expected2 = [1, 2, 3];
+
+const nums3 = [1, 1, 2, 3, 3, 4];
+const expected3 = [1, 2, 3, 4];
+
+// creat a new variable for the array
+// create a for loop to go through the current array (nums)
+// use the dedupe method to go through the array
+
+/**
+ * De-dupes the given sorted array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @return {Array<number>} The given array deduped.
+ */
+
+/**
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
+ */
+ function dedupeSorted(sortedNums) {
+  if (sortedNums.length <= 1) {
+    return sortedNums;
+  }
+
+  const dedupedArr = [];
+
+  for (let i = 0; i < sortedNums.length; i++) {
+    if (sortedNums[i] !== dedupedArr[dedupedArr.length - 1]) {
+      dedupedArr.push(sortedNums[i]);
+    }
+  }
+  return dedupedArr;
+}
+
+/**
+ * - Time: O(n) linear.
+ * - Space: O(2n) -> O(n) linear.
+ */
+function dedupeSortedHash(nums) {
+  if (nums.length == 0 || nums.length == 1) {
+    return nums;
+  }
+
+  const seen = {};
+  const dedupedArr = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const item = nums[i];
+
+    if (seen.hasOwnProperty(item) === false) {
+      seen[item] = true;
+      dedupedArr.push(item);
+    }
+  }
+  return dedupedArr;
+}
+
+/**
+ * - Time: O(2n) linear. One loop for the new set to iterate over the given
+ *    arr, then a second loop for the set to be spread back into an arr.
+ * - Space: O(n) linear.
+ */
+const dedupeSortedUsingSet = (sortedArr) => [...new Set(sortedArr)];
+
+/**
+ * - Time: O(2n) -> O(n) linear. The first loop is from the new Set having to
+ *    loop over the given array to create the Set.
+ * - Space: O(2n) -> O(n) linear. The has Set stores every num again at most in
+ *    the case of there being no dupes.
+ */
+function dedupeInPlace(nums) {
+  const numsSet = new Set(nums);
+
+  let i = 0;
+
+  for (const n of numsSet) {
+    // Overwrite the array in place.
+    nums[i++] = n;
+  }
+
+  // If there were dupes the size will be smaller, cut dupes off the end.
+  nums.length = numsSet.size;
+  return nums;
+}
+
+/**
+ * Without using a Set.
+ * - Time: O(2n) -> O(n) linear. Two loops at same level.
+ * - Space: O(2n) -> O(n) linear. The has table duplicates the nums stored.
+ */
+function dedupeUnorderedInPlace(nums) {
+  const isAddedTable = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    // Will convert to true when we add it while de-duping.
+    isAddedTable[nums[i]] = false;
+  }
+
+  let idxToOverwrite = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+
+    // If not added.
+    if (!isAddedTable[n]) {
+      nums[idxToOverwrite] = n;
+      isAddedTable[n] = true;
+      idxToOverwrite++;
+    }
+  }
+
+  // If there were dupes the array will be shorter, cut off dupes from the end.
+  nums.length = idxToOverwrite;
+  return nums;
+}
+/*****************************************************************************/
+
+/* 
+  Array: Mode
+  
+  Create a function that, given an array of ints,
+  returns the int that occurs most frequently in the array.
+  What if there are multiple items that occur the same number of time?
+    - return all of them (in an array)
+    - what if all items occur the same number of times?
+      - return empty array
+*/
+
+const nums1 = [];
+const expected1 = [];
+
+const nums2 = [1];
+const expected2 = [1];
+
+const nums3 = [5, 1, 4];
+const expected3 = [];
+
+const nums4 = [5, 1, 4, 1];
+const expected4 = [1];
+
+const nums5 = [5, 1, 4, 1, 5];
+const expected5 = [5, 1];
+//  - order doesn't matter
+
+/**
+ * Finds the mode or all modes if there are more than one. The mode is the
+ *    value which occurs the most times in the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Test
+ * @return {Array<number>} Mode or modes in any order.
+ */
+/**
+ * - Time: O(2n) -> O(n) linear.
+ * - Space: O(n) linear.
+ */
+ function mode(nums) {
+  if (nums.length === 1) {
+    return [nums[0]];
+  }
+
+  const modes = [];
+  const freq = {};
+  let maxFreq = 0;
+  let allSameFreq = true;
+
+  for (const n of nums) {
+    freq.hasOwnProperty(n) ? freq[n]++ : (freq[n] = 1);
+
+    if (freq[n] > maxFreq) {
+      maxFreq = freq[n];
+    }
+  }
+
+  for (const key in freq) {
+    if (freq[key] === maxFreq) {
+      // keys are strings, convert back to int
+      modes.push(parseInt(key));
+    } else {
+      allSameFreq = false;
+    }
+  }
+  // return empty array if allSameFreq, else return modes
+  return allSameFreq ? [] : modes;
+}
+
+module.exports = { mode };
+
+/*****************************************************************************/
